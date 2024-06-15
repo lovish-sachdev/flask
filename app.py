6ff154ad8a3b4c15e9e3c09579087c9f6276ff94
lovish-sachdev <lovishsachdev454@gmail.com>
2024-06-15 20:58:19 +0530

    faster stream

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 import base64
 from PIL import Image
 from io import BytesIO
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    data = request.json['image']
+@socketio.on('image')
+def handle_image(data):
     header, encoded = data.split(",", 1)
     image_data = base64.b64decode(encoded)
     image = Image.open(BytesIO(image_data))
@@ -19,7 +21,7 @@ def upload():
     buffered = BytesIO()
     flipped_image.save(buffered, format="PNG")
     flipped_image_data = base64.b64encode(buffered.getvalue()).decode('utf-8')
-    return jsonify({'flipped_image': f'data:image/png;base64,{flipped_image_data}'})
+    emit('flipped_image', f'data:image/png;base64,{flipped_image_data}')
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
