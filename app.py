f6bc61940e2b21f166e623b64d1008b93c44fe2f
lovish-sachdev <lovishsachdev454@gmail.com>
2024-06-15 18:26:54 +0530

    second commit

--- app.py
+++ app.py
@@ -1,29 +1,25 @@
-from flask import Flask, render_template, Response
-import cv2
+from flask import Flask, render_template, request, jsonify
+import base64
+from PIL import Image
+from io import BytesIO
 
 app = Flask(__name__)
-
-# Open the camera with appropriate index or path
-camera = cv2.VideoCapture(0)
-
-def generate_frames():
-    while True:
-        success, frame = camera.read()  # Read the camera frame
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/upload', methods=['POST'])
+def upload():
+    data = request.json['image']
+    header, encoded = data.split(",", 1)
+    image_data = base64.b64decode(encoded)
+    image = Image.open(BytesIO(image_data))
+    flipped_image = image.transpose(Image.FLIP_TOP_BOTTOM)
+    buffered = BytesIO()
+    flipped_image.save(buffered, format="PNG")
+    flipped_image_data = base64.b64encode(buffered.getvalue()).decode('utf-8')
+    return jsonify({'flipped_image': f'data:image/png;base64,{flipped_image_data}'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
